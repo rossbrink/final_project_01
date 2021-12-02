cb9213056a04481f5e42e4865660a88a0d148e0f
rossbrink <ross.brinkman@chicagobooth.edu>
2021-12-02 01:44:16 +0000

    Attempted model destroy 01

--- app/models/provider.rb
+++
@@ -1,5 +0,0 @@
-class Provider < ApplicationRecord
-  validates :email, :uniqueness => { :case_sensitive => false }
-  validates :email, :presence => true
-  has_secure_password
-end
--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  validates :email, :uniqueness => { :case_sensitive => false }
-  validates :email, :presence => true
-  has_secure_password
-end
